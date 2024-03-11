ad47a15a781a631baa8f8b046ef61e81568ab1c5
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-03-11 17:12:55 +0600

    test

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -1,94 +1,94 @@
-"use server";
+// "use server";
 
-import { revalidatePath } from "next/cache";
-import { prisma } from "../db/prism";
-import { handleError } from "./utils";
+// import { revalidatePath } from "next/cache";
+// import { prisma } from "../db/prism";
+// import { handleError } from "./utils";
 
-// CREATE
-export const createUser = async (user: any): Promise<any> => {
-  try {
-    console.log("🚀 ~ createUser ~ createUser:");
-    const newUser = await prisma.user.create({
-      data: user,
-    });
-    console.log("🚀 ~ createUser ~ newUser:", { newUser });
+// // CREATE
+// export const createUser = async (user: any): Promise<any> => {
+//   try {
+//     console.log("🚀 ~ createUser ~ createUser:");
+//     const newUser = await prisma.user.create({
+//       data: user,
+//     });
+//     console.log("🚀 ~ createUser ~ newUser:", { newUser });
 
-    return JSON.parse(JSON.stringify(newUser));
-  } catch (error) {
-    handleError(error);
-  }
-};
+//     return JSON.parse(JSON.stringify(newUser));
+//   } catch (error) {
+//     handleError(error);
+//   }
+// };
 
-// Red
-export const getUserById = async (
-  userId: string
-): Promise<IUser | undefined> => {
-  try {
-    const user = await prisma.user.findUnique({
-      where: {
-        clerkId: userId,
-      },
-    });
+// // Red
+// export const getUserById = async (
+//   userId: string
+// ): Promise<IUser | undefined> => {
+//   try {
+//     const user = await prisma.user.findUnique({
+//       where: {
+//         clerkId: userId,
+//       },
+//     });
 
-    console.log("🚀 ~ get UserBy Id:", { user });
+//     console.log("🚀 ~ get UserBy Id:", { user });
 
-    if (!user) throw new Error("User not found");
+//     if (!user) throw new Error("User not found");
 
-    return JSON.parse(JSON.stringify(user));
-  } catch (error) {
-    handleError(error);
-  }
-};
+//     return JSON.parse(JSON.stringify(user));
+//   } catch (error) {
+//     handleError(error);
+//   }
+// };
 
-// UPDATE
-export const updateUser = async (
-  clerkId: string,
-  user: any
-): Promise<IUser | undefined> => {
-  try {
-    const updatedUser = await prisma.user.update({
-      data: user,
-      where: {
-        clerkId,
-      },
-    });
+// // UPDATE
+// export const updateUser = async (
+//   clerkId: string,
+//   user: any
+// ): Promise<IUser | undefined> => {
+//   try {
+//     const updatedUser = await prisma.user.update({
+//       data: user,
+//       where: {
+//         clerkId,
+//       },
+//     });
 
-    console.log("🚀 ~ updated User by clerkId:", { updatedUser });
+//     console.log("🚀 ~ updated User by clerkId:", { updatedUser });
 
-    if (!updatedUser) throw new Error("User update failed");
+//     if (!updatedUser) throw new Error("User update failed");
 
-    return JSON.parse(JSON.stringify(updatedUser));
-  } catch (error) {
-    handleError(error);
-  }
-};
+//     return JSON.parse(JSON.stringify(updatedUser));
+//   } catch (error) {
+//     handleError(error);
+//   }
+// };
 
-// DELETE
-export const deleteUser = async (clerkId: string): Promise<any> => {
-  try {
-    // Find user to delete
-    const userToDelete = await prisma.user.findUnique({
-      where: {
-        clerkId: clerkId,
-      },
-    });
-    console.log("🚀 ~ deleteUser ~ userToDelete:", userToDelete);
+// // DELETE
+// export const deleteUser = async (clerkId: string): Promise<any> => {
+//   try {
+//     // Find user to delete
+//     const userToDelete = await prisma.user.findUnique({
+//       where: {
+//         clerkId: clerkId,
+//       },
+//     });
+//     console.log("🚀 ~ deleteUser ~ userToDelete:", userToDelete);
 
-    if (!userToDelete) {
-      throw new Error("User not found");
-    }
+//     if (!userToDelete) {
+//       throw new Error("User not found");
+//     }
 
-    // Delete user
-    const deletedUser = await prisma.user.delete({
-      where: {
-        id: userToDelete.id,
-      },
-    });
-    console.log("🚀 ~ deleteUser ~ deletedUser:", deletedUser);
-    revalidatePath("/");
+//     // Delete user
+//     const deletedUser = await prisma.user.delete({
+//       where: {
+//         id: userToDelete.id,
+//       },
+//     });
+//     console.log("🚀 ~ deleteUser ~ deletedUser:", deletedUser);
+//     revalidatePath("/");
 
-    return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
-  } catch (error) {
-    handleError(error);
-  }
-};
+//     return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
+//   } catch (error) {
+//     handleError(error);
+//   }
+// };
