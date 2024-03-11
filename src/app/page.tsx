83816e48cae6f0024669b5eeac81c6079bb9a72c
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-03-12 02:17:06 +0600

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import { UserButton } from "@clerk/nextjs";
 import { NextPage } from "next";
 
 import { Counter } from "@/components/Counter";
+import { Button } from "@/components/ui/button";
 
 const Home: NextPage = () => {
   return (
     <main className="flex flex-col justify-center items-center h-screen gap-5 bg-slate-950 w-full text-slate-300">
       <UserButton />
+      <Button className="bg-orange-500 hover:bg-orange-600">Button</Button>
       <Counter />
     </main>
   );
