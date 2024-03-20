665c71a2b67186e29e8ad3f9400f56e0dc3507e3
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-03-20 23:16:23 +0600

    solve error

--- src/components/Breadcrumb.tsx
+++ src/components/Breadcrumb.tsx
@@ -6,15 +6,18 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import { useTranslations } from "next-intl";
 import { FC } from "react";
 
 export type IProps = { pre: { label: string; link: string }; current: string };
 export const CustomBreadcrumb: FC<IProps> = ({ current, pre }) => {
+  const t = useTranslations("HomePage");
+
   return (
     <Breadcrumb>
       <BreadcrumbList>
         <BreadcrumbItem>
-          <BreadcrumbLink href="/">Home</BreadcrumbLink>
+          <BreadcrumbLink href="/">{t("page-label")}</BreadcrumbLink>
         </BreadcrumbItem>
         <BreadcrumbSeparator />
         <BreadcrumbItem>
