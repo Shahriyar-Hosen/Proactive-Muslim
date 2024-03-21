24e6816cc1e5fc6757aeef4e0cd604ebf5de4c01
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-03-22 04:52:23 +0600

    clean up

--- src/app/[locale]/(pages)/test/page.tsx
+++ src/app/[locale]/(pages)/test/page.tsx
@@ -1,57 +1,51 @@
-"use client";
+import { NextPage } from "next";
+import { useTranslations } from "next-intl";
+import { unstable_setRequestLocale } from "next-intl/server";
 
-import { toast } from "sonner";
-
-import { RoleGate } from "@/components/auth/role-gate";
-import { FormSuccess } from "@/components/form/form-success";
+import { APIsCheck } from "@/components/test/APIsCheck";
+import { Admin } from "@/components/test/Admin";
+import { Counter } from "@/components/test/Counter";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { admin } from "@/server/actions/auth/admin";
-import { UserRole } from "@prisma/client";
 
-const AdminPage = () => {
-  const onServerActionClick = () => {
-    admin().then((data) => {
-      if (data.error) {
-        toast.error(data.error);
-      }
+const AdminPage: NextPage<ParamsLocale> = ({ params: { locale } }) => {
+  // Enable static rendering
+  unstable_setRequestLocale(locale);
 
-      if (data.success) {
-        toast.success(data.success);
-      }
-    });
-  };
-
-  const onApiRouteClick = () => {
-    fetch("/api/admin").then((response) => {
-      if (response.ok) {
-        toast.success("Allowed API Route!");
-      } else {
-        toast.error("Forbidden API Route!");
-      }
-    });
-  };
+  const t = useTranslations("HomePage");
 
   return (
-    <Card className="w-[600px]">
-      <CardHeader>
-        <p className="text-2xl font-semibold text-center">🔑 Admin</p>
-      </CardHeader>
-      <CardContent className="space-y-4">
-        <RoleGate allowedRole={UserRole.ADMIN}>
-          <FormSuccess message="You are allowed to see this content!" />
-        </RoleGate>
-        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
-          <p className="text-sm font-medium">Admin-only API Route</p>
-          <Button onClick={onApiRouteClick}>Click to test</Button>
-        </div>
+    <main className="flex flex-col justify-center items-center gap-5 relative">
+      <APIsCheck />
 
-        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
-          <p className="text-sm font-medium">Admin-only Server Action</p>
-          <Button onClick={onServerActionClick}>Click to test</Button>
-        </div>
-      </CardContent>
-    </Card>
+      <section className="space-y-5">
+        <h1 className="text-3xl font-semibold leading-tight tracking-tight text-white md:text-5xl">
+          {t("title")}
+        </h1>
+        <p className="max-w-[590px]">
+          {t.rich("description", {
+            code: (chunks) => (
+              <code className="font-mono text-cyan-400">{chunks}</code>
+            ),
+          })}
+        </p>
+      </section>
+
+      <div className="space-y-6 text-center">
+        <h1 className="text-6xl font-semibold text-primary drop-shadow-md">
+          🔐 Auth
+        </h1>
+
+        <Button variant={"destructive"}>destructive</Button>
+        <Button variant={"ghost"}>ghost</Button>
+        <Button variant={"link"}>link</Button>
+        <Button variant={"outline"}>outline</Button>
+        <Button variant={"secondary"}>secondary</Button>
+        <p className="text-white text-lg">A simple authentication service</p>
+      </div>
+      <Counter />
+
+      <Admin />
+    </main>
   );
 };
 
