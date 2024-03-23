213489e74cca2a30aded20067e9050d452c33ef8
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-03-23 20:20:22 +0600

    loading

--- src/app/[locale]/loading.tsx
+++ src/app/[locale]/loading.tsx
@@ -1,11 +1,15 @@
-import { SingleRoundSkeleton } from "@/components/clock/single-round-clock";
 import { Skeleton } from "@/components/ui/skeleton";
 
 const Loading = () => {
   return (
     <main className="flex flex-col space-y-3 justify-center items-center">
       <Skeleton className="h-[80px] w-full rounded-xl" />
-      <SingleRoundSkeleton />
+      <div className="w-full py-5 grid grid-cols-3 gap-5 justify-self-center justify-items-center place-items-center place-content-center">
+        <Skeleton className="w-full bg-slate-800/80 rounded-md h-[250px]" />
+        <Skeleton className="w-full bg-slate-800/80 rounded-md h-[250px]" />
+        <Skeleton className="w-full bg-slate-800/80 rounded-md h-[250px]" />
+      </div>
+      {/* <SingleRoundSkeleton /> */}
       {/* <ThreeRoundedClockSkeleton /> */}
       <div className="space-y-2 flex justify-between items-center gap-5">
         <Skeleton className="h-80 w-full" />
