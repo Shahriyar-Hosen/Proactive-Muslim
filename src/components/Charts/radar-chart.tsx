f2cb43fcaba17e17598904f703b738f3fe21e4df
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-04-05 11:19:00 +0600

    prv

--- src/components/Charts/radar-chart.tsx
+++ src/components/Charts/radar-chart.tsx
@@ -92,7 +92,6 @@ const ChartRadar: FC<IChartRadar> = ({
       width={width}
       height={height}
       outerRadius={outer}
-      className="bg-slate-800/[0.5] backdrop-blur-sm rounded-xl w-[200px] lg:w-[380px] h-full shadow-inner"
     />
   </ResponsiveContainer>
 );
