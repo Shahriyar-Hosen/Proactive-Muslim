97dd7752d29d7aca633e155cf06344335bd64de8
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-04-09 11:19:00 +0600

    prv

--- src/components/Charts/radar-chart.tsx
+++ src/components/Charts/radar-chart.tsx
@@ -96,7 +96,6 @@ const ChartRadar: FC<IChartRadar> = ({
     >
       <Tooltip content={<CustomizedTooltip />} />
       <PolarGrid />
-      <PolarAngleAxis dataKey="namaz" stroke="#e2e8f0" fill="#0eca2d" />
     </RadarChart>
   </ResponsiveContainer>
 );
